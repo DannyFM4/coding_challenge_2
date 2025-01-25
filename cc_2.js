84efa9e70b28c4f6499ea94a93d775d36c99cd21
DannyFM4 <danielfelicianomunoz@usf.edu>
2025-01-24 20:35:04 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+//Task 1: Working with Arrays
+let products = ["Shampoo", "Conditioner", "Body Soap", "Face Wash", "Towel"] //Creating an Array
+products.push("Toothpaste") //Adding the string toothpaste to my array
+products.pop() //deleting the last item in the array, in this case toothpaste
+console.log(products) //logging the array
